751d236119a4465d1c3d26eb4da41466f7d5105d
swt02026 <swt02026@gmail.com>
2018-02-03 23:24:25 +0800

    alert for error url

--- www/js/app.js
+++ www/js/app.js
@@ -1,13 +1,9 @@
-var resultDiv;
 function startScan() {
   cordova.plugins.barcodeScanner.scan(
       (result) => {
-        var s = 'Result: ' + result.text + '<br/>' +
-            'Format: ' + result.format + '<br/>' +
-            'Cancelled: ' + result.cancelled;
-        resultDiv.html(s);
         let url = new URL(result.text);
         if (url.hostname === 'fish.mowei.com.tw') window.location = result.text;
+        alert('Error QRCode: ' + url.hostname);
       },
       (error) => {
         alert('Scanning failed: ' + error);
@@ -16,7 +12,6 @@ function startScan() {
 
 function init() {
   $('#startScan').click(startScan);
-  resultDiv = $('#results');
 }
 
 document.addEventListener('deviceready', init, false);
